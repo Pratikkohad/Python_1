70415d14c410ebd58130abab18da136e18512cab
PratikKohad <kohad.1995@gmail.com>
2021-11-16 08:45:56 +0530

    Basics

---
+++ Python_Workspace/Python_1/First_package/Arthematic_Operation.py
@@ -0,0 +1,10 @@
+a=10
+b=20
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%10)
+print(a**2)
+print(a//b)
---
+++ Python_Workspace/Python_1/First_package/String Concatenation.py
@@ -0,0 +1,7 @@
+a= "pratik"
+b="Kohad"
+c=10
+d=100
+
+print("Types 1 :- "+a+" "+b)
+
